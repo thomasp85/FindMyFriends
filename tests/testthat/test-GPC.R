c9000acfca8b6f24e51123dd3898393704598dcb
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2015-08-27 16:53:00 +0200

    Add GPC test

--- tests/testthat/test-GPC.R
+++ tests/testthat/test-GPC.R
@@ -1,11 +1,20 @@
 ## TBD
 # Include gpcGrouping, recurseCompare, recurseCompPar, fillTree, cutK
 
-context("GPC")
+context("GPC and helpers")
 
-## TODO: Rename context
-## TODO: Add more tests
+pg <- .loadPgExample()
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("gpcGrouping works", {
+    set.seed(1)
+    pg1 <- gpcGrouping(pg)
+    set.seed(1)
+    pg2 <- gpcGrouping(pg, lowMem=T)
+    cacheDir <- tempdir()
+    set.seed(1)
+    pg3 <- gpcGrouping(pg, cacheDB=cacheDir)
+    pg4 <- gpcGrouping(pg, cacheDB=cacheDir)
+    expect_equal(pg1, pg2)
+    expect_equal(pg2, pg3)
+    expect_equal(pg3, pg4)
 })
