5f84c111752211451fca7e609c12da51f9240128
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2015-12-09 15:00:09 +0100

    Template linearKernel (plus cleanup)

--- src/linearKernel.cpp
+++ src/linearKernel.cpp
@@ -1,38 +1,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// static int *ptrP;
-// static int *ptrI;
-// static double *ptrX;
-
-//[[Rcpp::export]]
-List linearKernel(IntegerVector pX, IntegerVector jX, 
-                  NumericVector xX, IntegerVector selX,
-                  double lowerLimit, double upperLimit) {
+template<typename T1, typename T2, typename R1, typename R2>
+IntegerVector linearKernel(const T1& pX, const T1& jX, const T2& xX, const T1& selX, 
+                  double lowerLimit, double upperLimit, R1& P, R1& I, R2& X) {
     int i, j, i1, j1, ind1, ind2, nextMember, nGroups;
     double kv;
     
     int sizeX = selX.size();
-    
-    // set initial size and growth factor for i and x arrays
-    uint64_t vectorSize = sizeX * 2;
-//     double growBy = 1.6;
-//     
-//     // int *pptr = (int *) Calloc(sizeX + 1, int);
-//     int *iptr = (int *) Calloc(vectorSize, int);
-//     double *xptr = (double *) Calloc(vectorSize, double);
-//     // ptrP = pptr;
-//     ptrI = iptr;
-//     ptrX = xptr;
-//     
-//     int nextFree = 0;
-    
-    std::deque<int> P;
-    std::deque<int> I;
-    std::deque<double> X;
-//     P.reserve(sizeX + 1);
-//     I.reserve(vectorSize);
-//     X.reserve(vectorSize);
     
     // Initialize grouping variables
     std::vector<int> group; // Group representative
@@ -42,9 +17,6 @@ List linearKernel(IntegerVector pX, IntegerVector jX,
     I.push_back(0);
     X.push_back(1.0);
     P.push_back(0);
-    // pptr[nextFree] = 0;
-//     iptr[nextFree] = 0;
-//     xptr[nextFree++] = 0;
     member[0] = 0;
     std::vector<double> kValues(sizeX, -1); // Temporary storage for kv until it's decided if new group should be created
     
@@ -85,27 +57,13 @@ List linearKernel(IntegerVector pX, IntegerVector jX,
             member[j] = nextMember;
         } else {
             P.push_back(I.size());
-            // P.push_back(nextFree);
             nGroups = group.size();
-//             if (((uint64_t) nextFree + nGroups) > vectorSize) {
-//                 // realloc arrays i and x
-//                 vectorSize += nGroups;
-//                 vectorSize *= growBy;
-//                 iptr = (int *) Realloc(iptr, vectorSize, int);
-//                 xptr = (double *) Realloc(xptr, vectorSize, double);
-//                 ptrI = iptr;
-//                 ptrX = xptr;
-//             }
             for(i = 0; i < nGroups; i++) {
                 if (kValues[i] > lowerLimit) {
                     I.push_back(i);
                     X.push_back(kValues[i]);
-//                     iptr[nextFree] = i;
-//                     xptr[nextFree++] = kValues[i];
                 }
             }
-//             iptr[nextFree] = nGroups;
-//             xptr[nextFree++] = 1.0;
             I.push_back(nGroups);
             X.push_back(1.0);
             group.push_back(j1);
@@ -113,21 +71,9 @@ List linearKernel(IntegerVector pX, IntegerVector jX,
         }
     }
     P.push_back(I.size());
-//     P.push_back(nextFree);
-//     
-//     
-//     IntegerVector I;
-//     NumericVector X;
-//     if (nextFree > 0) {
-//         I = IntegerVector(iptr, iptr + nextFree);
-//         X = NumericVector(xptr, xptr + nextFree);
-//     }
-//     
-//     // Release memory
-//     Free(iptr);
-//     ptrI = NULL;
-//     Free(xptr);
-//     ptrX = NULL;
+    
+    return member;
+}
     
     return List::create(
         Named("member") = member,
