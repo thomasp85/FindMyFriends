f44a08349784a131a9b7fc07e6a0554261c1eec0
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2016-03-03 10:38:54 +0000

    Merge branch 'master' into devel
    From: Thomas Dybdal Pedersen <thomasp85@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/FindMyFriends@114206 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-similarities.R
+++ tests/testthat/test-similarities.R
@@ -3,10 +3,11 @@ context("Similarity calculations work")
 pg <- .loadPgExample()
 
 test_that("kmerSimilarity works", {
-    #sim1 <- kmerSimilarity(pg, lowMem=T, kmerSize=4, lowerLimit=0.8) # kebabs error/crash on ubuntu
+    skip_on_os('win')
+    sim1 <- kmerSimilarity(pg, lowMem=T, kmerSize=4, lowerLimit=0.8)
     sim2 <- kmerSimilarity(pg, lowMem=F, kmerSize=4, lowerLimit=0.8)
     sim3 <- kmerSimilarity(pg, lowMem=F, kmerSize=4, lowerLimit=0.8, pParam=BiocParallel::SerialParam())
-    #expect_equal(sim1, sim2)
+    expect_equal(sim1, sim2)
     expect_equal(sim2, sim3)
     expect_is(sim2, 'dgCMatrix')
     expect_equal(dim(sim2), rep(nGenes(pg), 2))
